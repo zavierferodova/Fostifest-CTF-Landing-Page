42086efc492cd3b4dc4f16ff2e6e0cfcdb49ba25
Zavier Ferodova Al Fitroh <zavierferodova@gmail.com>
2022-10-05 10:07:19 +0700

    Add mobile nav menu

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     AOS.init()
+
+    const navButton = document.querySelector('.mobile-nav-button')
+    const mobileNav = document.querySelector('.mobile-nav-menu')
+
+    navButton.onclick = () => {
+        const isClosed = mobileNav.classList.contains('hidden')
+
+        if (isClosed) {
+            mobileNav.classList.remove('hidden')
+            mobileNav.classList.add('flex')
+        } else {
+            mobileNav.classList.remove('flex')
+            mobileNav.classList.add('hidden')
+        }
+    }
 })
